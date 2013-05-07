96a83151aca4711704c2bd779325b4479ffc39e1
Pinak Saha <Pinaksaha90@gmail.com>
2013-05-07 18:12:11 -0400

    multidirectory generation added

---
+++ api/response_server.c
@@ -0,0 +1,24 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/file.h>
+#include <dirent.h> 
+#include <sys/stat.h>  
+#include <string.h>
+#include <pthread.h>
+
+
+int main(int argc, char * argv[])
+{
+	
+	
+	
+	return 0;
+}
